330522284120cd07ade18d99aeda45ce116c719a
Kenny Victoria <kvict3@gmail.com>
2016-05-07 18:17:54 -0400

    js not working and html fixed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,8 +4,8 @@
 	 
 	function displayVillainInfo(){
 	
-	var superv = $(this).data('superv');
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + superv + "&api_key=dc6zaTOxFJmzC&limit=10";
+	var superv = $(this).attr('data-name');
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + villains + "&api_key=dc6zaTOxFJmzC&limit=10";
 
         $.ajax({
                 url: queryURL,
@@ -16,9 +16,20 @@
             	 var results = response.data;
 
             for (var i = 0; i < results.length; i++) {
-            	 	var villainDiv = $('<div>');
+            	 	var villainDiv = $('<div class="villain">');
 
-                    var p = $('<p>').text("Rating: " + results[i].rating);
+            	 	var rating = response.Rated
+
+					var p = $('<p>').text( "Rating: " + rating);
+
+					villainDiv.append(p);
+
+					var image = $('<img>');
+					image.attr('src', results[i].images.fixed_height.url);
+
+					villainDiv.append(image);
+
+					$('#supervillain').prepend(villainDiv);
 
 
                     //--------------------------------
@@ -31,19 +42,19 @@
 	function renderButtons(){ 
 
 		// Deletes the movies prior to adding new movies (this is necessary otherwise you will have repeat buttons)
-		$('#buttonsView').empty();
+		$('#superButtons').empty();
 
 		// Loops through the array of movies
-		for (var i = 0; i < movies.length; i++){
+		for (var i = 0; i < villains.length; i++){
 
 			// Then dynamicaly generates buttons for each movie in the array
 
 			// Note the jQUery syntax here... 
 		    var a = $('<button>') // This code $('<button>') is all jQuery needs to create the beginning and end tag. (<button></button>)
-		    a.addClass('movie'); // Added a class 
-		    a.attr('data-name', movies[i]); // Added a data-attribute
-		    a.text(movies[i]); // Provided the initial button text
-		    $('#buttonsView').append(a); // Added the button to the HTML
+		    a.addClass('villains'); // Added a class 
+		    a.attr('data-name', villains[i]); // Added a data-attribute
+		    a.text(villains[i]); // Provided the initial button text
+		    $('#superButtons').append(a); // Added the button to the HTML
 		}
 	}
 
@@ -53,29 +64,28 @@
 	$('#addSuper').on('click', function(){
 
 		// This line of code will grab the input from the textbox
-		var superv = $('#movie-input').val().trim();
+		var superv = $('#super-input').val().trim();
 
-		// The movie from the textbox is then added to our array
-		movies.push(movie);
+		// The villain from the textbox is then added to our array
+		villains.push(superv);
 		
 		// Our array then runs which handles the processing of our movie array
 		renderButtons();
 
 		// We have this line so that users can hit "enter" instead of clicking on ht button and it won't move to the next page
 		return false;
-	})
- $('#supervillain').on('click', function(){
 
- 	 var state = $(this).attr('data-state');
+ 	 	var state = $(this).attr('data-state');
 
- 	 if ( state == 'still'){
+ 	 	if ( state == 'still'){
                 $(this).attr('src', $(this).data('animate'));
                 $(this).attr('data-state', 'animate');
             }else{
                 $(this).attr('src', $(this).data('still'));
                 $(this).attr('data-state', 'still');
             }
-     });
-
- // This calls the renderButtons() function
+        });
+	$(document).on('click', '.villain', displayVillainInfo);
 	renderButtons();
+     };
+
